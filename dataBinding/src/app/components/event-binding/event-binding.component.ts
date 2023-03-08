c197be4ebf8c896f5fd457732073689beacf9ad4
Okami_PL <szymon.galuszka.114@gmail.com>
2023-03-08 13:47:55 +0100

    event binding

--- dataBinding/src/app/components/event-binding/event-binding.component.ts
+++ dataBinding/src/app/components/event-binding/event-binding.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
   templateUrl: './event-binding.component.html',
   styleUrls: ['./event-binding.component.css']
 })
-export class EventBindingComponent {
+export class EventBindingComponent implements OnInit {
+  brand: string;
+  model: string;
+  imageUrl: string;
 
+  constructor() {
+    this.brand = "Audi";
+    this.model = "RS6";
+    this.imageUrl = "https://image.shutterstock.com/z/stock-photo-london-england-audi-rs-avant-performance-estate-car-parked-in-mayfair-ditrict-of-694886881.jpg"
+  }
+
+  ngOnInit() {
+  }
+
+  buyCar($event: any) {
+      console.log(`Kupiłeś ${this.brand} ${this.model}`, $event);
+  }
 }
