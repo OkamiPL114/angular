b52758fa648e30fc4d842cc8cc16b9e3afe79546
Okami_PL <szymon.galuszka.114@gmail.com>
2023-03-08 14:26:31 +0100

    2 komponenty

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
