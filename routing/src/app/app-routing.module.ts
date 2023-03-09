457edd4101e2a86d7f80d6342c901160e3be164b
Okami_PL <szymon.galuszka.114@gmail.com>
2023-03-09 10:46:28 +0100

    pod-ścieżki z about

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { RealizationsComponent } from './components/realizations/realizations.component';
+import { LocationsComponent } from './components/locations/locations.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent, children: [
+    { path: 'realizations', component: RealizationsComponent},
+    { path: 'locations', component: LocationsComponent}
+  ]}
 ];
 
 @NgModule({
