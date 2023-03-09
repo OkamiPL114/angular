d21b59a17f333d14008f650973bd4ed931025c70
Okami_PL <szymon.galuszka.114@gmail.com>
2023-03-09 10:06:14 +0100

    reactive form

--- forms/src/app/components/reactive-form/reactive-form.component.ts
+++ forms/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit{
+  carForm!: FormGroup;
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  onSubmit() {
+    console.log(this.carForm);
+  }
+
+  onClear() {
+    this.carForm?.reset();
+  }
+
+  private initializeForm() {
+    this.carForm = new FormGroup({
+      'carBrand': new FormControl("Audi R8", Validators.required),
+      'carDescription': new FormControl(null),
+      'carAmount': new FormControl(null)
+    });
+  }
 }
